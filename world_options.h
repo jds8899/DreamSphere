7ac1a9949ce87984d6863c6db46a0987d9577788
jds8899 <jds8899@rit.edu>
2019-02-28 23:26:00 -0500

    Changed all files to use C style code. Split main out into multiple context source files.

--- world_options.h
+++ world_options.h
@@ -1,12 +1,14 @@
-#pragma once
-class WorldOptions
-{
-public:
-	WorldOptions(unsigned size, unsigned tess);
-	~WorldOptions();
-	unsigned size();
-	unsigned tess();
-private:
-	unsigned size_, tess_;
-};
+#ifndef WORLD_OPTIONS_H
+#define WORLD_OPTIONS_H
 
+#include <kos.h>
+
+typedef struct {
+	unsigned size, tess;
+} WorldOptions;
+
+WorldOptions* world_options_init(unsigned size, unsigned tess);
+
+void world_options_cleanup(WorldOptions* wo);
+
+#endif
--- world_va.cpp
+++ world_va.cpp
@@ -1,73 +1,60 @@
 #include "world_va.h"
 
-
-WorldVA::WorldVA(WorldOptions wo) :
-	verts_(),
-	norms_(),
-	texs_(),
-	size_(),
-	tess_(),
-	vert_per_edge_(),
-	vert_count_() {
-		size_			= wo.size();
-		tess_			= wo.tess();
-		vert_per_edge_	= size_ * tess_;
-		vert_count_		= vert_per_edge_ * vert_per_edge_;
-		verts_			= (vector_t**)malloc((vert_per_edge_) * sizeof(vector_t*));
-		norms_			= (vector_t**)malloc((vert_per_edge_) * sizeof(vector_t*));
-		texs_			= (vector_t**)malloc((vert_per_edge_) * sizeof(vector_t*));
-
-		for (unsigned i = 0; i < vert_per_edge_; ++i) {
-			verts_[i] = (vector_t*)malloc((vert_per_edge_ * 2 + 2) * sizeof(vector_t));
-			norms_[i] = (vector_t*)malloc((vert_per_edge_ * 2 + 2) * sizeof(vector_t));
-			texs_[i]  = (vector_t*)malloc((vert_per_edge_ * 2 + 2) * sizeof(vector_t));
-		}
-
-		build_world_();
-}
+void build_world_(WorldVA* wva);
 
 
-WorldVA::~WorldVA() {
-	for (unsigned i = 0; i < vert_per_edge_; ++i) {
-		free(verts_[i]);
-		free(norms_[i]);
-		free(texs_[i]);
+WorldVA* world_init(WorldOptions* wo) {
+	WorldVA* ret = (WorldVA*)malloc(sizeof(WorldVA));
+
+	unsigned size			= wo->size;
+	unsigned tess			= wo->tess;
+	unsigned vert_per_edge	= size * tess;
+	ret->size				= size;
+	ret->tess				= tess;
+	ret->vert_per_edge		= vert_per_edge;
+	ret->vert_count			= vert_per_edge * vert_per_edge;
+	ret->verts				= (vector_t**)malloc((vert_per_edge) * sizeof(vector_t*));
+	ret->norms				= (vector_t**)malloc((vert_per_edge) * sizeof(vector_t*));
+	ret->texs				= (vector_t**)malloc((vert_per_edge) * sizeof(vector_t*));
+
+	for (unsigned i = 0; i < vert_per_edge; ++i) {
+		ret->verts[i] = (vector_t*)malloc((vert_per_edge * 2 + 2) * sizeof(vector_t));
+		ret->norms[i] = (vector_t*)malloc((vert_per_edge * 2 + 2) * sizeof(vector_t));
+		ret->texs[i]  = (vector_t*)malloc((vert_per_edge * 2 + 2) * sizeof(vector_t));
 	}
-	free(verts_);
-	free(norms_);
-	free(texs_);
+
+	build_world_(ret);
+	return ret;
 }
 
-unsigned WorldVA::strip_num() {
-	return vert_per_edge_;
+
+void world_cleanup(WorldVA* wva) {
+	for (unsigned i = 0; i < wva->vert_per_edge; ++i) {
+		free(wva->verts[i]);
+		free(wva->norms[i]);
+		free(wva->texs[i]);
+	}
+	free(wva->verts);
+	free(wva->norms);
+	free(wva->texs);
+	free(wva);
+	wva = NULL;
 }
 
-unsigned WorldVA::strip_size() {
-	return vert_per_edge_ * 2 + 2;
-}
+void build_world_(WorldVA* wva) {
+	unsigned tess			= wva->tess;
+	unsigned size			= wva->size;
+	unsigned vert_per_edge	= wva->vert_per_edge;
+	float    step			= 1.0f / tess;
 
-vector_t** WorldVA::verts() {
-	return verts_;
-}
-
-vector_t** WorldVA::norms() {
-	return norms_;
-}
-
-vector_t** WorldVA::texs() {
-	return texs_;
-}
-
-void WorldVA::build_world_() {
-	float step = 1.0f / tess_;
-	for (unsigned i = 0; i < vert_per_edge_; ++i) {
+	for (unsigned i = 0; i < vert_per_edge; ++i) {
 		//printf("STRIP %x/%x\n", i + 1, vert_per_edge_);
-		for (unsigned j = 0; j < vert_per_edge_ + 1; ++j) {
+		for (unsigned j = 0; j < vert_per_edge + 1; ++j) {
 
-			float x0	=  i * step - size_ / 2.0f;
-			float x1	= (i + 1) * step - size_ / 2.0f;
+			float x0	=  i * step - size / 2.0f;
+			float x1	= (i + 1) * step - size / 2.0f;
 			float y		=  0.0f;
-			float z		=  j * step - size_ / 2.0f;
+			float z		=  j * step - size / 2.0f;
 
 			float u0	= (i * step) / 2.0f;
 			float u1	= (i + 1) * step / 2.0f;
@@ -84,14 +71,14 @@ void WorldVA::build_world_() {
 			vector_t t0 = {u0, v, 0.0f, 1.0f};
 			vector_t t1 = {u1, v, 0.0f, 1.0f};
 
-			verts_[i][j * 2]		= p0;
-			verts_[i][j * 2 + 1]	= p1;
+			wva->verts[i][j * 2]		= p0;
+			wva->verts[i][j * 2 + 1]	= p1;
 
-			norms_[i][j * 2]		= n;
-			norms_[i][j * 2 + 1]	= n;
+			wva->norms[i][j * 2]		= n;
+			wva->norms[i][j * 2 + 1]	= n;
 
-			texs_[i][j * 2]			= t0;
-			texs_[i][j * 2 + 1]		= t1;
+			wva->texs[i][j * 2]			= t0;
+			wva->texs[i][j * 2 + 1]		= t1;
 		}
 	}
 }
